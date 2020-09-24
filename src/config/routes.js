80d38d8abbd538a132ebcefa23fd75182988446a
Zzzen <mail@emmanuelprice.com>
2020-09-24 11:45:34 -0400

    Add homepage

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,16 @@
 import Quotes from '../views/Quotes.js';
+import Home from '../views/Home.js';
 
 const routes = [
     {
         view: Quotes,
         path: '/selected-quotes',
         linkText: 'Selected Quotes'
+    },
+    {
+        view: Home,
+        path: '/',
+        linkText: 'EZP'
     }
 ];
 
---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => ( 
+    <div className="Home">
+        <div className="content">
+            <h1 className="name">Emmanuel Z. Price</h1>
+            <ul className="links">
+                <li>v</li>
+                <li>w</li>
+                <li>x</li>
+                <li>y</li>
+                <li>z</li>
+            </ul>
+        </div>
+    </div>
+);
+
+export default Home;
